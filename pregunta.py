5886b6e94e0615403a5c166f9825480b189c3a1f
sebstianzamora <69611081+sebstianzamora@users.noreply.github.com>
2024-09-01 20:37:45 -0500

    limpieza de datos

--- pregunta.py
+++ pregunta.py
@@ -17,4 +17,21 @@ def clean_data():
     # Inserte su código aquí
     #
 
+    # eliminar duplicados
+    df = df.drop_duplicates()
+    
+    # eliminación de filas con valores faltantes
+    df = df.dropna()
+    
+    # estandarizar
+    # convertir texto a minúsculas y eliminar espacios adicionales en columnas de texto
+    df.columns = df.columns.str.strip().str.lower()
+    for col in df.select_dtypes(include=['object']).columns:
+        df[col] = df[col].str.strip().str.lower()
+    
     return df
+
+#dtf = clean_data()
+#print(dtf)
+    
+    
